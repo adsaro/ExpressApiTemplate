c9b12e4b40f0c03c55dc16d657895b246bdc5c9b
adsaro <adsaro@ciencias.unam.mx>
2020-07-28 14:37:34 -0500

    Proyecto funcionando con ES6

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const indexRouter = express.Router();
+
+indexRouter.get('/', (req, res) =>
+  res.status(200).json({ message: 'Bienvenido al template de Express' })
+);
+
+export default indexRouter;
