578c8755f34cc5739305f0488e49b54ef937f20b
adsaro <adsaro@ciencias.unam.mx>
2020-07-28 20:13:05 -0500

    eslint y prettier instalados y funcionando correctamente

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ import express from 'express';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) =>
-  res.status(200).json({ message: 'Bienvenido al template de Express' })
-);
+indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Bienvenido al template de Express' }));
 
 export default indexRouter;
